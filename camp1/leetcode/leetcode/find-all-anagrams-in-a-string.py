145fee000e2bd8bae4d77dbf08291f788b04c804
Bezawit Tezera <121764038+BezawitTezera@users.noreply.github.com>
2024-03-13 06:39:40 -0700

    Add solution for Find All Anagrams in a String

--- camp1/leetcode/leetcode/find-all-anagrams-in-a-string.py
+++ camp1/leetcode/leetcode/find-all-anagrams-in-a-string.py
@@ -2,6 +2,7 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s):
             return []
+            
         Scount = defaultdict(int)
         Pcount = Counter(p)
         k = len(p)
@@ -19,7 +20,7 @@ class Solution:
                 del Scount[s[i - k]]
 
             Scount[s[i]] += 1
-            
+
         if Pcount == Scount:
             ans.append(len(s)-k)
         
