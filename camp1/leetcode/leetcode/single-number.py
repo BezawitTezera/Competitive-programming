08d2f3cffc87c9a39fb5f3f92100d3a9b01557b1
Bezawit Tezera <121764038+BezawitTezera@users.noreply.github.com>
2024-03-18 14:15:23 -0700

    Add solution for Single Number

---
+++ camp1/leetcode/leetcode/single-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        for key, value in counter.items():
+            if value == 1:
+                return key
